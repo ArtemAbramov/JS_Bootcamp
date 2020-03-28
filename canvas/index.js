4e5a583b52f4ebe9612bba732aeecae85109c429
Artem Abramov <tanukisuicide@yandex.ru>
2020-03-28 17:57:53 +0300

    Write generator random labyrinth

--- canvas/index.js
+++ canvas/index.js
@@ -6,9 +6,11 @@ const {
   Bodies
 } = Matter
 
-const cells = 3
+const cells = 6
 const width = 600
 const height = 600
+
+const unitLength = width / cells
 
 const engine = Engine.create()
 const {world} = engine
@@ -36,6 +38,18 @@ const walls = [
 World.add(world, walls)
 
 // Maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter)
+    counter--
+    [arr[counter], arr[index]] = [arr[index], arr[counter]]
+  }
+  return arr
+}
+
 const grid = Array(cells)
     .fill(null)
     .map(() => Array(cells).fill(false))
@@ -47,3 +61,83 @@ const verticals = Array(cells)
 const horizontals = Array(cells - 1)
     .fill(null)
     .map(() => Array(cells).fill(false))
+
+const startRow = Math.floor(Math.random() * cells)
+const startColumn = Math.floor(Math.random() * cells)
+
+const stepThroughCell = (row, column) => {
+  if (grid[row][column] === true) {
+    return
+  }
+  grid[row][column] = true
+  const neighbors = shuffle([
+      [row - 1, column, 'up'],
+      [row, column + 1, 'right'],
+      [row + 1, column, 'down'],
+      [row, column - 1, 'left']
+  ])
+
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor
+
+    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+      continue
+    }
+
+    if (grid[nextRow][nextColumn]) {
+      continue
+    }
+
+    if (direction === 'left') {
+      verticals[row][column - 1] = true
+    } else if (direction === 'right') {
+      verticals[row][column] = true
+    } else if (direction === 'up') {
+      horizontals[row - 1][column] = true
+    } else if (direction === 'down') {
+      horizontals[row][column] = true
+    }
+
+    stepThroughCell(nextRow, nextColumn)
+  }
+}
+
+stepThroughCell(startRow, startColumn)
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return
+    }
+
+    const wall = Bodies.rectangle(
+        columnIndex * unitLength + unitLength / 2,
+        rowIndex * unitLength + unitLength,
+        unitLength,
+        10,
+        {
+          isStatic: true
+        }
+    )
+    World.add(world, wall)
+  })
+})
+
+verticals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return
+    }
+
+    const wall = Bodies.rectangle(
+        columnIndex * unitLength + unitLength,
+        rowIndex * unitLength + unitLength / 2,
+        10,
+        unitLength,
+        {
+          isStatic: true
+        }
+    )
+    World.add(world, wall)
+  })
+})
