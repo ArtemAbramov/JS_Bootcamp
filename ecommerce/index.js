e3310388ea11d84482903e02caac8ce62589e977
Artem Abramov <tanukisuicide@yandex.ru>
2020-04-23 16:38:36 +0300

    Refactor: restructure routes and added templating

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,82 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cokieSessoin = require('cookie-session')
-const usersRepo = require('./repositories/users')
+const cookieSession = require('cookie-session')
+const authRouter = require('./routes/admin/auth')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cokieSessoin({
+app.use(cookieSession({
     keys: ['asdasde343fwsdfai8a']
 }))
-
-app.get('/signup', (req, res) => {
-    res.send(`
-<div>
-    ${req.session.userId}
-    <form method="POST">
-        <input name="email" type="email" placeholder="email">
-        <input name="password" type="password" placeholder="password">
-        <input name="passwordConfirmation" type="password" placeholder="password confirmation">
-        <button>Sign Up</button>
-    </form>        
-</div>
-    `)
-})
-
-app.post('/signup', async (req, res) => {
-    const { email, password, passwordConfirmation } = req.body
-
-    const existingUser = await usersRepo.getOneBy({ email })
-    if (existingUser) {
-        return res.send('Email in use')
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match')
-    }
-
-    const user = await usersRepo.create({ email, password })
-
-    req.session.userId = user.id
-
-    res.send(JSON.stringify(user, null, 2))
-})
-
-app.get('/signout', (req, res) => {
-    req.session = null
-    res.send('You are logged out')
-})
-
-app.get('/signin', (req, res) => {
-    res.send(`
-<div>
-    <form method="POST">
-        <input name="email" type="email" placeholder="email">
-        <input name="password" type="password" placeholder="password">
-        <button>Sign In</button>
-    </form>        
-</div>
-    `)
-})
-
-app.post('/signin', async (req, res) => {
-    const { email, password } = req.body
-
-    const user = await usersRepo.getOneBy({ email })
-
-    if (!user) {
-        return res.send('Email not found')
-    }
-
-    if (user.password !== password) {
-        return res.send('Invalid password')
-    }
-
-    req.session.userId = user.id
-
-    res.send('You are signed in!!!')
-})
+app.use(authRouter)
 
 app.listen(3000, () => {
     console.log('Server is started')
